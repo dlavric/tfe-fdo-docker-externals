8a61be6f2ce5676c9bcf0990d218612806fe4b29
dlavric <daniela@hashicorp.com>
2023-11-27 16:11:42 +0100

    partial code added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tfe-fdo-docker
 This repository will install TFE FDO Docker https://developer.hashicorp.com/terraform/enterprise/flexible-deployments/install/docker/install
+
+
+
+What do I need:
+
+- [ ] AWS Instance that has 4 CPU and 16GB minimum as per [official requirements](https://developer.hashicorp.com/terraform/enterprise/replicated/architecture/reference-architecture/aws#terraform-enterprise-server-ec2-via-auto-scaling-group)
+- [ ] DNS hostname for the server
+- [ ] Valid certificates created with Let's encrypt, [see this Github repo](https://github.com/dlavric/create-certificates) that will be incorporated here
+- [ ] External Services: Postgres & S3 bucket   
---
+++ main.tf
@@ -0,0 +1,172 @@
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "daniela-vpc"
+  }
+}
+
+resource "aws_subnet" "publicsub" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "daniela-public-subnet"
+  }
+}
+
+
+resource "aws_internet_gateway" "internetgw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "daniela-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internetgw.id
+  }
+
+  tags = {
+    Name = "daniela-route"
+  }
+}
+
+
+resource "aws_iam_role" "daniela-role" {
+  name = "daniela-role-docker"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "daniela-profile" {
+  name = "daniela-profile-docker"
+  role = aws_iam_role.daniela-role.name
+}
+
+resource "aws_iam_role_policy" "daniela-policy" {
+  name = "daniela-policy-docker"
+  role = aws_iam_role.daniela-role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "s3:ListBucket",
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::*/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_instance" "instance" {
+  ami                  = "ami-0694d931cee176e7d" # eu-west-1
+  instance_type        = "t2.xlarge"
+  iam_instance_profile = aws_iam_instance_profile.daniela-profile.name
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+
+  key_name = "daniela-fdo-key"
+
+  root_block_device {
+    volume_size = 50
+  }
+
+  #   user_data = templatefile("terraform_ent.yaml", {
+  #     #consul_bootstrap_expect = var.consul_server_count,
+  #     license = "license.rli"
+  #   })
+
+  network_interface {
+    network_interface_id = aws_network_interface.nic.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "daniela-tfe-fdodocker"
+  }
+
+}
+
+resource "aws_security_group" "securitygp" {
+
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description = "https-access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
+
+  ingress {
+    description = "ssh-access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc.cidr_block]
+  }
+
+  egress {
+    description = "egress-rule"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    type = "daniela-security-group"
+  }
+}
+
+
+resource "aws_network_interface" "nic" {
+  subnet_id       = aws_subnet.publicsub.id
+  security_groups = [aws_security_group.securitygp.id]
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.securitygp.id
+  network_interface_id = aws_instance.instance.primary_network_interface_id
+}
+
+resource "aws_eip" "eip" {
+  instance = aws_instance.instance.id
+  domain   = "vpc"
+}
+
